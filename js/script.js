a7cb5787dcd443dcbcc22decb29459161891aecd
Jose Marques <pilotmarquess@gmail.com>
2019-05-01 17:10:11 -0500

    completes add skill feature

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-// We are going to make a Dev Skills App
+// We're going to make a Dev Skills App
 // We need for a user to type in a dev skill
 // Then we will get the value of the skill
 // We need to add functionality to the button so that we get the value
-// We will build a dom element using that value
+// We will build a DOM element using that value
 // Then we will add that element to the DOM
 
 
-// state Variables
-let $inputVal
+// State Variables
+let $inputVal, $skillElement
 
 // Event Listeners
 $('button').on('click', getInputText)
 
-//Functions
+// Functions
 function getInputText(){
-    alert('Button Clicked')
+    $inputVal = $('input').val()
+    $('input').val('')
+    createElement($inputVal)
 }
+
+function createElement(str){
+    $skillElement = $(`<li>${str}</li>`)
+    addSkill($skillElement)
+}
+
+function addSkill(element){
+    $('section ul').append(element)
+}
+
+
+
+
